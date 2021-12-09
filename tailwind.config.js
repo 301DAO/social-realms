aefe33d841a1bee463f588985df4be711540499c
omar-aziz <omars@hey.com>
2021-12-09 17:01:34 -0600

    tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+ // mode: "jit",
+//  purge: ["./src/**/**/*.{js,jsx,ts,tsx}", "./src/pages/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
