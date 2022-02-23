37489551ab4134af655dbacac52df14ab3d1b5ff
omar-aziz <omars@hey.com>
2022-02-23 10:55:02 -0600

    middleware debug on vercel

--- src/pages/nfts/_middleware.ts
+++ src/pages/nfts/_middleware.ts
@@ -8,41 +8,54 @@ type Params = {
   address: string;
 };
 
-export async function middleware(req: NextRequest) {
+export default async function middleware(req: NextRequest) {
   const { address: _addressOrName } = req.page.params as Params;
 
-  const { url: sourceUrl } = req;
-  const rewriteUrl = new URL('/invalid', sourceUrl).toString();
-  let rewriteResponse = NextResponse.rewrite(rewriteUrl);
+  const url = req.nextUrl.clone();
 
-  /**
-   * This is all super fast (23:31) and will rule out most bad input
-   * Doing this first to prevent having to interact with ethers if possible
-   **/
-  const valExists = valueExists(_addressOrName);
-  const startsWith0x = _addressOrName?.indexOf('0x') !== -1;
-  const endsWithEth = _addressOrName?.indexOf('.eth') !== -1;
+  const { url: sourceUrl, nextUrl } = req;
 
-  if (!valExists || (!startsWith0x && !endsWithEth)) {
+  //const rewriteUrl = new URL('/invalid', sourceUrl).toString();
+  url.pathname = '/invalid';
+  let rewriteResponse = NextResponse.rewrite(url); // NextResponse.rewrite(rewriteUrl);
+
+  try {
+    /**
+     * This is all super fast (23:31) and will rule out most bad input
+     * Doing this first to prevent having to interact with ethers if possible
+     **/
+    const valExists = valueExists(_addressOrName);
+    const startsWith0x = _addressOrName?.indexOf('0x') !== -1;
+    const endsWithEth = _addressOrName?.indexOf('.eth') !== -1;
+
+    if (!valExists || (!startsWith0x && !endsWithEth)) {
+      return rewriteResponse;
+    }
+
+    // we already check the type up there so casting here so it doesn't bug us
+    const addressOrName = _addressOrName as string;
+
+    // it's a valid eth address, proceed
+    if (utils.isAddress(addressOrName)) {
+      console.info(`test vercel log: utils.isAddress line 40`, utils.isAddress(addressOrName));
+      return NextResponse.next();
+    }
+
+    const provider = wagmiProvider();
+    console.info(`test vercel log: provider line 45`, provider);
+    const resolvedAddress = await provider.resolveName(addressOrName);
+    console.info(`test vercel log: resolvedAddress line 47`, resolvedAddress);
+    // it's a valid ENS name, proceed
+    if (resolvedAddress && utils.isAddress(resolvedAddress)) {
+      console.info(`test vercel log: utils.isAddress`, utils.isAddress(resolvedAddress));
+      return NextResponse.next();
+    }
+
+    // sike, get out
     return rewriteResponse;
+  } catch (error) {
+    console.info(`test vercel log: error line 57`, error);
+    console.error(`Error in /nfts/_middleware: `, error instanceof Error ? error.message : error);
   }
-
-  // we already check the type up there so casting here so it doesn't bug us
-  const addressOrName = _addressOrName as string;
-
-  // it's a valid eth address, proceed
-  if (utils.isAddress(addressOrName)) {
-    return NextResponse.next();
-  }
-
-  const provider = wagmiProvider();
-  const resolvedAddress = await provider.resolveName(addressOrName);
-
-  // it's a valid ENS name, proceed
-  if (resolvedAddress && utils.isAddress(resolvedAddress)) {
-    return NextResponse.next();
-  }
-
-  // sike, get out
   return rewriteResponse;
 }
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,16 +1,13 @@
 declare namespace NodeJS {
   export interface ProcessEnv extends NodeJS.ProcessEnv {
     VERCEL_URL: string; // System environment variable when deployed on Vercel: https://vercel.com/docs/environment-variables#system-environment-variables
+    DATABASE_URL: string;
     ACCESS_TOKEN_NAME: string;
     ACCESS_TOKEN_SECRET: string;
     NEXT_PUBLIC_ALCHEMY: string;
     NEXT_PUBLIC_ALCHEMY_KEY: string;
     NEXT_PUBLIC_ETHERSCAN_KEY: string;
-    NEXT_PUBLIC_INFURA: string;
     NEXT_PUBLIC_INFURA_PROJECT_ID: string;
-    NEXT_PUBLIC_INFURA_ENDPOINT_MAINNET: string;
-
-    DATABASE_URL: string;
     NEXT_PUBLIC_NFT_PORT_KEY: string;
     NEXT_PUBLIC_COVALENT_API_KEY: string;
   }
