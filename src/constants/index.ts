c94d4af4afaac5c0a442f16fe6942bfcc6040d92
omar-aziz <omars@hey.com>
2022-03-17 22:30:53 -0500

    vercel url.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,4 @@ export * from './network';
 const PORT = process.env.PORT || process.env.NEXT_PUBLIC_DEV_PORT;
 
 export const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? `${process.env.VERCEL_URL}:${PORT}`
-    : `http://localhost:${PORT}`;
+  process.env.NODE_ENV === 'development' ? `http://localhost:${PORT}` : `${process.env.VERCEL_URL}`;
